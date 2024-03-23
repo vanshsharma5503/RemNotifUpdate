e7da6870352169218e8fff06d568e02dd9fa5a83
vansh sharma <vb2810@srmist.edu.in>
2024-03-23 22:36:21 +0530

    first commit

---
+++ REM/REMApp.swift
@@ -0,0 +1,17 @@
+//
+//  REMApp.swift
+//  REM
+//
+//  Created by SHHH!! private on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct REMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RemindersView()
+        }
+    }
+}
